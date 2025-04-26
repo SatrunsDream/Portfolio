660bb51847614c93df085a1d94be5354047e9cd1
Sardor Sobirov <sardorbsobirov@gmail.com>
2025-04-25 18:57:25 -0700

    lab 04

--- global.js
+++ global.js
@@ -1,7 +1,8 @@
 console.log("IT'S ALIVE!");
 
-
-const BASE_PATH = "https://satrunsdream.github.io/Portfolio/";
+const BASE_PATH = window.location.hostname.includes("github.io")
+  ? "https://satrunsdream.github.io/Portfolio/"
+  : "/";
 
 let pages = [
   { url: '', title: 'Home' },
@@ -91,3 +92,29 @@ form?.addEventListener('submit', (event) => {
   const mailtoUrl = `${form.action}?${params.toString()}`;
   location.href = mailtoUrl; // Open the mailto URL
 });
+
+export async function fetchJSON(url) {
+  try {
+    const response = await fetch(url);
+    if (!response.ok) {
+      throw new Error(`Failed to fetch projects: ${response.statusText}`);
+    }
+    const data = await response.json();
+    return data;
+  } catch (error) {
+    console.error('Error fetching or parsing JSON data:', error);
+  }
+}
+
+export function renderProjects(projects, containerElement, headingLevel = 'h2') {
+  containerElement.innerHTML = '';
+  for (const project of projects) {
+    const article = document.createElement('article');
+    article.innerHTML = `
+      <${headingLevel}>${project.title}</${headingLevel}>
+      <img src="${project.image}" alt="${project.title}">
+      <p>${project.description}</p>
+    `;
+    containerElement.appendChild(article);
+  }
+}
---
+++ index.js
@@ -0,0 +1,19 @@
+import { fetchJSON, renderProjects } from './global.js';
+
+const projects = await fetchJSON('./lib/projects.json');
+const latestProjects = projects.slice(0, 3);
+const projectsContainer = document.querySelector('.projects');
+renderProjects(latestProjects, projectsContainer, 'h2');
+
+const profileStats = document.querySelector('#profile-stats');
+if (profileStats) {
+  const githubData = await fetchJSON('https://api.github.com/users/SatrunsDream');
+  profileStats.innerHTML = `
+    <dl>
+      <dt>Public Repos:</dt><dd>${githubData.public_repos}</dd>
+      <dt>Public Gists:</dt><dd>${githubData.public_gists}</dd>
+      <dt>Followers:</dt><dd>${githubData.followers}</dd>
+      <dt>Following:</dt><dd>${githubData.following}</dd>
+    </dl>
+  `;
+}
