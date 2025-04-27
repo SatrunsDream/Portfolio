c72b87b4d2aacb9a9bcd4772ac6b39e3c3a664b5
Sardor Sobirov <sardorbsobirov@gmail.com>
2025-04-26 20:11:41 -0700

    gitpage

--- index.js
+++ index.js
@@ -9,11 +9,24 @@ const profileStats = document.querySelector('#profile-stats');
 if (profileStats) {
   const githubData = await fetchJSON('https://api.github.com/users/SatrunsDream');
   profileStats.innerHTML = `
-    <dl>
-      <dt>Public Repos:</dt><dd>${githubData.public_repos}</dd>
-      <dt>Public Gists:</dt><dd>${githubData.public_gists}</dd>
-      <dt>Followers:</dt><dd>${githubData.followers}</dd>
-      <dt>Following:</dt><dd>${githubData.following}</dd>
-    </dl>
+    <h2>My GitHub Stats</h2>
+    <div class="github-stats">
+      <div>
+        <h3>Followers</h3>
+        <p>${githubData.followers}</p>
+      </div>
+      <div>
+        <h3>Following</h3>
+        <p>${githubData.following}</p>
+      </div>
+      <div>
+        <h3>Public Repos</h3>
+        <p>${githubData.public_repos}</p>
+      </div>
+      <div>
+        <h3>Public Gists</h3>
+        <p>${githubData.public_gists}</p>
+      </div>
+    </div>
   `;
 }
